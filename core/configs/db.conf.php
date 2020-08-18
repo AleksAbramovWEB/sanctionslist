0e9579abf71c5a328800b3e978cf18f9b1942011
AleksAbramovWEB <59263535+AleksAbramovWEB@users.noreply.github.com>
2020-08-18 14:42:19 +0200

    1.2

--- core/configs/db.conf.php
+++ core/configs/db.conf.php
@@ -7,11 +7,7 @@ return [
     'db_name' => 'reference_books',
     'charset' => 'UTF8'
 ];
-//    'host' => 'mysql.zzz.com.ua',
-//    'user' => 'aleksabramovweb',
-//    'password' => '19901508Oi1',
-//    'db_name' => 'aleksabramovweb',
-//    'charset' => 'UTF8'
+
 
 
  ?>
